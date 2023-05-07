7b6bf36613efa9f27c530c4311e727788404fa84
moyashimegane <sunsunmoon7014@gmail.com>
2023-05-07 16:04:39 +0900

    [update]モデルの設定とリレーションの設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,32 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
+        'screen_name',
         'name',
+        'profile_image',
         'email',
-        'password',
+        'password'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function followers() {
+        return $this->belongsToMany(self::class, 'followers', 'followed_id', 'following_id');
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function follows() {
+        return $this->belongsToMany(self::class, 'followers', 'following_id', 'followed_id');
+    }
 }
